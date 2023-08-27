38c2d6f2e65e905c153ed3a8eca07cff9964030d
dee0922 <143274137+dee0922@users.noreply.github.com>
2023-08-27 13:34:28 +0530

    Add files via upload

---
+++ app_java/src/app_java/Calculator.java
@@ -0,0 +1,53 @@
+package app_java;
+import java.util.Scanner;
+
+	public class Calculator {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Simple Calculator");
+	        System.out.print("Enter the first number: ");
+	        double num1 = scanner.nextDouble();
+
+	        System.out.print("Enter the second number: ");
+	        double num2 = scanner.nextDouble();
+
+	        System.out.println("Select an operation:");
+	        System.out.println("1. Addition");
+	        System.out.println("2. Subtraction");
+	        System.out.println("3. Multiplication");
+	        System.out.println("4. Division");
+	        System.out.print("Enter operation choice (1/2/3/4): ");
+	        int choice = scanner.nextInt();
+
+	        double result = 0;
+
+	        switch (choice) {
+	            case 1:
+	                result = num1 + num2;
+	                break;
+	            case 2:
+	                result = num1 - num2;
+	                break;
+	            case 3:
+	                result = num1 * num2;
+	                break;
+	            case 4:
+	                if (num2 != 0) {
+	                    result = num1 / num2;
+	                } else {
+	                    System.out.println("Cannot divide by zero.");
+	                    System.exit(0);
+	                }
+	                break;
+	            default:
+	                System.out.println("Invalid choice.");
+	                System.exit(0);
+	        }
+
+	        System.out.println("Result: " + result);
+
+	        scanner.close();
+	    }
+	}
+
